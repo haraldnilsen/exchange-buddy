43863e428f2e769e7c05d549fbe52c4f7ae61ae3
haraldnilsen <harald_998@hotmail.com>
2023-03-15 14:50:22 +0100

    added router-elements

--- student-frontend/src/index.tsx
+++ student-frontend/src/index.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const route = createBrowserRouter([
+  {
+    path:"/",
+    element: <Home/>
+  },
+  {
+    path:"/login",
+    element: <Login/>
+  }
+])
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route}/>
   </React.StrictMode>
 );
 
