758a387a8f6735b6eeef4793ccdc12b5ddfbc12b
Amund Fremming <AmundFremming@158-37-224-146.hvl.no>
2023-03-15 14:53:08 +0100

    Kakemann

---
+++ student-frontend/src/pages/Home.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+// Simple home page for the webapp
+
---
+++ student-frontend/src/pages/Search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+/*
+    Search page for the different search options
+    Switches between two different search components
+        - SearchRoom
+        - SearchUser
+*/
