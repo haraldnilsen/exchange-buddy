9a68fa3c00a95de1b91757a88cc006ad673b87c4
D596242 <Dennisosmani@hotmail.com>
2023-03-22 19:45:35 +0100

    testerssss

--- student-backend/src/test/java/exchange/student/ServiceTests.java
+++ student-backend/src/test/java/exchange/student/ServiceTests.java
@@ -11,26 +11,39 @@ import exchange.student.entity.Userr;
 import exchange.student.repository.UserrRepo;
 import exchange.student.service.UserrService;
 
+/**
+ * 
+ * @author Dennis Osmani
+ *
+ */
+
 public class ServiceTests {
 
 	// TEST DATA
 	@Autowired UserrRepo urepo;
 	@Autowired UserrService uservice;
 	private Userr user1;
+	private Userr user2;
 	
 	@BeforeEach()
 	public void setup() {
 				
 		user1 = uservice.RegisterUserr("12345678", "Petter", "Pilgaard", "20.12.1969", "mordiNaken", "Mann", "Vendela");
-		urepo.save(user1);
+		user2 = new Userr("12345678", "Petter", "Pilgaard", "20.12.1969", "mordiNaken", "Mann", "123", "Vendela");
+		urepo.save(user2);
 	}
 	
+	/**
+	 * Test if the users mobile is in the database
+	 */
 	
 	@Test
 	public void findUserrByMobile() {
 		System.out.println("Test av userr fra databasen");
 		
-		assertEquals("123456789", uservice.getUserr(user1.getMobile()));
+//		assertEquals("123456789", uservice.getUserr(user1.getMobile()));
+		
+		assertTrue(user2.equals(uservice.getUserr(user2.getMobile())));
 	}
 	
 }
