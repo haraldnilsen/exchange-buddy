93d854be362e53fbe31e33b4c4c7c5b3c77efaf7
Amund Fremming <AmundFremming@Finns-MacBook-Pro-2.local>
2023-03-28 16:11:47 +0200

    kakemann

---
+++ student-frontend/src/util/RoomPostService.tsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// For searching rooms
+const ROOM_GET_API_URL = "http://localhost:8080/api/roompost";
+const ROOM_GET_ID_API_URL = "http://localhost:8080/api/roompost";
+
+// For handling rooms
+const ROOM_POST_API_URL = "http://localhost:8080/api/roompost";
+const ROOM_UPDATE_API_URL = "http://localhost:8080/api/roompost";
